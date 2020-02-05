e5ab2781cd6cc72c4f66b39eecd54612de708709
JimusHimus <georgii123456789@yandex.ru>
2020-02-05 15:08:02 +0300

    chmod

--- deploy.pl
+++ deploy.pl
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Content-type: text/plain";
 echo '';
+chmod -R 755 /var/www/domains/m.chats.qzo.su
 cd /var/www/domains/m.chats.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull https://github.com/JimusHimus/Chats > /dev/null
