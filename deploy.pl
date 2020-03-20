fb0c134e4f8316f4db813e6da87a5e2cb8edc0c6
JimusHimus <39002687+JimusHimus@users.noreply.github.com>
2020-03-20 18:32:03 +0300

    Delete deploy.pl

--- deploy.pl
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo -e "Content-type: text/plain\n";
-cd /var/www/domains/m.chats.qzo.su/ || exit > /dev/null
-git reset --hard 4691ab095f79ab4d65ff782de3b81c8bfb587516 > /dev/null
-git pull https://github.com/JimusHimus/Chats > /dev/null
-chmod -R 755 /var/www/domains/m.chats.qzo.su
